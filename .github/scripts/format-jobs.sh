1ce8b2463a84a68411bd4e00daf39321a3bce5f0
Mark Coleman <markcoleman@gmail.com>
2025-04-26 18:06:08 -0400

    Enhance Workflow Timing Collector to Send Formatted Job and Step Data to New Relic (#22)
    * fixes
    
    * gettting closer
    
    * fixes
    
    * manual mode as well

--- .github/scripts/format-jobs.sh
+++ .github/scripts/format-jobs.sh
@@ -6,7 +6,9 @@ LATEST_RUN_ID="$2"
 REPO="$3"
 WORKFLOW_NAME="$4"
 
-jq -c '.jobs[] | {
+rm -f formatted_jobs.json
+
+jq -c '[.jobs[] | select(.steps != null) | { 
   repository: "'$REPO'",
   workflow_name: "'$WORKFLOW_NAME'",
   run_id: "'$LATEST_RUN_ID'",
@@ -16,13 +18,20 @@ jq -c '.jobs[] | {
   job_conclusion: .conclusion,
   job_started_at: .started_at,
   job_completed_at: .completed_at,
-  steps: [
-    .steps[] | {
-      step_name: .name,
-      step_status: .status,
-      step_conclusion: .conclusion,
-      step_started_at: .started_at,
-      step_completed_at: .completed_at
-    }
-  ]
-}' "$INPUT_FILE" > formatted_jobs.json
+  steps: .steps
+} as $job | $job.steps[] | {
+  repository: $job.repository,
+  workflow_name: $job.workflow_name,
+  run_id: $job.run_id,
+  job_id: $job.job_id,
+  job_name: $job.job_name,
+  job_status: $job.job_status,
+  job_conclusion: $job.job_conclusion,
+  job_started_at: $job.job_started_at,
+  job_completed_at: $job.job_completed_at,
+  step_name: .name,
+  step_status: .status,
+  step_conclusion: .conclusion,
+  step_started_at: .started_at,
+  step_completed_at: .completed_at
+}]' "$INPUT_FILE" > formatted_jobs.json
